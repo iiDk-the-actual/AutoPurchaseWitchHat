f44028c4c5bd78c97bd61018d70f1dec47e00466
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-28 00:33:02 -0400

    1.1.0

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.goldentrophy.gorillatag.autopurchasewitchhat";
         public const string Name = "AutoPurchaseWitchHat";
-        public const string Version = "1.0.0";
+        public const string Version = "1.1.0";
     }
 }
